edf6d387d0086d0a185b8945593e9b7013f54fff
Eevee <eevee.git@veekun.com>
2018-12-05 19:45:43 -0800

    Fix screen size scaling logic
    I implemented letterboxing for Anise game ages ago, but never upstreamed
    it; whoops!  This is basically the same idea, but with some more
    explicit support for a "minimum" size.

--- game.lua
+++ game.lua
@@ -15,7 +15,8 @@ local Game = Object:extend{
     -- FIXME this seems ugly, but the alternative is to have sprite.lua implicitly depend here
     sprites = SpriteSet._all_sprites,
     is_dirty = false,
-    max_screen_size = Vector(960, 540),  -- half of 1080p; 30 x ~17 32x32 tiles
+    native_size = Vector(960, 540),  -- half of 1080p; 30 x ~17 32x32 tiles
+    minimum_size = Vector(832, 448),  -- allow cropping ~2x4 tiles (optional)
 
     size = nil,  -- vector of the game's intrinsic size
     scale = nil,  -- how much the game is scaled up on display
@@ -45,14 +46,23 @@ end
 -- Game resolution
 
 function Game:_determine_scale()
-    local w, h = love.graphics.getDimensions()
-    self.scale = math.ceil(math.max(
-        w / self.max_screen_size.x,
-        h / self.max_screen_size.y))
-    local width = math.ceil(w / self.scale)
-    local height = math.ceil(h / self.scale)
-    self.size = Vector(width, height)
-    self.screen = AABB(0, 0, w, h)
+    local sw, sh = love.graphics.getDimensions()
+    -- FIXME do something globally useful when the window is too small!
+    -- This scales as large as possible without exceeding the min size.
+    -- In the face of ambiguity (e.g. when min size is much smaller than native
+    -- size, there's a choice between more cropping or more letterboxing), it
+    -- prefers showing more of the game.
+    local target_size = self.minimum_size or self.native_size
+    self.scale = math.floor(math.min(sw / target_size.x, sh / target_size.y))
+
+    local gw = math.min(math.ceil(sw / self.scale), self.native_size.x)
+    local gh = math.min(math.ceil(sh / self.scale), self.native_size.y)
+    self.size = Vector(gw, gh)
+    self.screen = AABB(
+        math.floor((sw - gw * self.scale) / 2),
+        math.floor((sh - gh * self.scale) / 2),
+        gw * self.scale,
+        gh * self.scale)
 end
 
 function Game:getDimensions()
